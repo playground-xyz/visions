d515127b82ee04dc3f7dd7de9e51bedc864ee147
Julian Fell <jtfell@Julians-MacBook-Pro.local>
2017-04-26 09:44:29 +1000

    add skip, limit and sort methods

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
-const joinjs = require('join-js').default;
-const _ = require('lodash');
 
 const preProcessModels = require('./lib/preProcessModels');
 const constructQuery = require('./lib/constructQuery');
+const doJoins = require('./lib/doJoins');
 
 /**
  * Factory function for adding our own "populate" syntax to knex
@@ -21,6 +20,7 @@ KnexJoins.prototype.applyViews = function (views) {
 };
 
 /**
+ * Create a new prepared query based on the chosen model.
  *
  * @param {String} modelId
  */
@@ -28,13 +28,38 @@ KnexJoins.prototype.selectModel = function (modelId) {
   return new PreparedQuery(modelId, this.processedModels, this.knex);
 };
 
+/**
+ * Get the name of the view that has been applied to the model.
+ *
+ * @param {String} modelId
+ */
+KnexJoins.prototype.getView = function (modelId) {
+  const model = this.models.find(model => model.mapId === modelId);
+  return model.viewId;
+};
 
+/**
+ * Object that presents a simple interface for constructing queries for a
+ * relational database using a model map object describing the connections
+ * between models.
+ *
+ * @method populate
+ * @method where
+ * @method skip
+ * @method limit
+ * @method sort
+ * @method exec
+ *
+ */
 function PreparedQuery(modelId, models, knex) {
 
-  this.models = _.cloneDeep(models);
+  // This is safe as the models object is serialisable
+  this.models = JSON.parse(JSON.stringify((models)));
+
   this.knex = knex;
   this.modelId = modelId;
   this.populated = [];
+  this.whereClauses = [];
 
   this.originalModel = this.models.find(model => model.mapId === modelId);
   if (!this.originalModel) {
@@ -44,51 +69,42 @@ function PreparedQuery(modelId, models, knex) {
   return this;
 }
 
-
-PreparedQuery.prototype.doJoin = function (res) {
-  
-  // Manipulate the models being passed into join-js to un-populate the collections/associations of
-  // associations/collections
-  this.models = this.models.map(model => {
-    const assocFound = this.originalModel.associations.find(assoc => assoc.mapId === model.mapId);
-    const collFound = this.originalModel.collections.find(coll => coll.mapId === model.mapId);
-    if (collFound || assocFound) {
-      model.properties = model.properties.concat(
-          model.associations.map(assoc => assoc.mapId)
-      );
-      model.properties = model.properties.concat(
-          model.collections.map(coll => coll.mapId)
-      );
-      model.collections = [];
-      model.associations = [];
-    }
-    return model;
-  });
-
-  // Do the populating
-  let joined = joinjs.map(res, this.models, this.modelId);
-
-  // Simplify the collections that shouldnt be populated
-  this.modifiedModel.collections.forEach(coll => {
-    if (!coll.populate) {
-      joined = joined.map(nested => {
-        nested[coll.name] = nested[coll.name].map(c => c.id);
-        return nested;
-      });
-    }
-  });
-
-  return joined;
-};
-
 PreparedQuery.prototype.populate = function (name) {
-
   this.populated.push(name);
   // Chainable
   return this;
 };
 
-PreparedQuery.prototype.prepareJoins = function () {
+PreparedQuery.prototype.skip = function (value) {
+  this._skip = value
+
+  // Chainable
+  return this;
+};
+
+PreparedQuery.prototype.limit = function (value) {
+  this._limit = value
+
+  // Chainable
+  return this;
+};
+
+PreparedQuery.prototype.where = function (clauses) {
+  this.whereClauses = this.whereClauses.concat(clauses);
+
+  // Chainable
+  return this;
+};
+
+PreparedQuery.prototype.sort = function (attr, dir) {
+  this.sortAttr = attr;
+  this.sortDir = dir;
+
+  // Chainable
+  return this;
+};
+
+PreparedQuery.prototype.exec = function () {
   
   this.nonPopulated = [];
 
@@ -99,7 +115,7 @@ PreparedQuery.prototype.prepareJoins = function () {
     idProperty: this.originalModel.idProperty,
     properties: this.originalModel.properties,
 
-    // Filter out an
+    // Add a flag to determine which connections will need populating
     associations: this.originalModel.associations.map(assoc => {
       assoc.populate = !!this.populated.find(name => name === assoc.name);
       return assoc;
@@ -110,6 +126,20 @@ PreparedQuery.prototype.prepareJoins = function () {
     })
   };
 
-  // Return the knex object so other things can be added
-  return constructQuery(this.models, this.modifiedModel, this.knex);
+  // Construct the query, run it and convert it to a nested object
+  return constructQuery(
+      this.models,
+      this.modifiedModel,
+      this.knex,
+      this.whereClauses,
+      this.sortAttr,
+      this.sortDir
+  ).then(data => doJoins(
+      data,
+      this.models,
+      this.originalModel,
+      this.modifiedModel,
+      this._skip,
+      this._limit
+  ));
 };
